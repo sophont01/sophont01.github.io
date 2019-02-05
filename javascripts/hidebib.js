324197cc92cb8807b047216c2b6e922408407763
sophont01 <saurabh0saini@gmail.com>
2019-02-05 23:56:49 +0530

    withPubs

---
+++ javascripts/hidebib.js
@@ -0,0 +1,45 @@
+// from: http://www.robots.ox.ac.uk/~vedaldi/assets/hidebib.js
+function hideallbibs()
+{
+    var el = document.getElementsByTagName("div") ;
+    for (var i = 0 ; i < el.length ; ++i) {
+        if (el[i].className == "paper") {
+            var bib = el[i].getElementsByTagName("pre") ;
+            if (bib.length > 0) {
+                bib [0] .style.display = 'none' ;
+            }
+        }
+    }
+}
+
+function togglebib(paperid)
+{
+    var paper = document.getElementById(paperid) ;
+    var bib = paper.getElementsByTagName('pre') ;
+    //var bib = paper.getElementsByTagName('code') ;
+    if (bib.length > 0) {
+        if (bib [0] .style.display == 'none') {
+            bib [0] .style.display = 'block' ;
+		bib[0] .swapStyleSheet
+        } else {
+            bib [0] .style.display = 'none' ;
+        }
+    }
+}
+
+function toggleblock(blockId)
+{
+   hideallbibs();
+   var block = document.getElementById(blockId);
+   if (block.style.display == 'none') {
+    block.style.display = 'block' ;
+   } else {
+    block.style.display = 'none' ;
+   }
+}
+
+function hideblock(blockId)
+{
+   var block = document.getElementById(blockId);
+   block.style.display = 'none' ;
+}
